f33a234a4e493c06f0d034f6d442bcf18f9a400b
shuu11 <shuukuubas@gmail.com>
2021-04-10 23:15:53 +0900

    cocoon child

--- functions.php
+++ functions.php
@@ -5,3 +5,28 @@ if ( !defined( 'ABSPATH' ) ) exit;
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
+// webp許可
+function custom_mime_types( $mimes ) {
+    $mimes['webp'] = 'image/webp';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'custom_mime_types' );
+
+	// メディアでWebP画像サムネイル表示
+    function webp_is_displayable($result, $path) {
+        if ($result === false) {
+            $displayable_image_types = array( IMAGETYPE_WEBP );
+            $info = @getimagesize( $path );
+     
+            if (empty($info)) {
+                $result = false;
+            } elseif (!in_array($info[2], $displayable_image_types)) {
+                $result = false;
+            } else {
+                $result = true;
+            }
+        }
+        return $result;
+    }
+    add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
+    
