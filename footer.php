6b191017f1aa954d9a7d2af882472d665960d846
shuu11 <shuukuubas@gmail.com>
2021-04-30 00:21:40 +0900

    cocoon child

---
+++ footer.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit; ?>
+          </main>
+
+        <?php get_sidebar(); ?>
+
+      </div>
+
+    </div>
+
+    <?php
+    ////////////////////////////
+    //コンテンツ下部ウィジェット
+    ////////////////////////////
+    if ( is_active_sidebar( 'content-bottom' ) ) : ?>
+    <div id="content-bottom" class="content-bottom wwa">
+      <div id="content-bottom-in" class="content-bottom-in wrap">
+        <?php dynamic_sidebar( 'content-bottom' ); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+
+    <?php //投稿パンくずリストがフッター手前の場合
+    if ((is_single() || is_category()) && is_single_breadcrumbs_position_footer_before()){
+      get_template_part('tmp/breadcrumbs');
+    } ?>
+
+    <?php //固定ページパンくずリストがフッター手前の場合
+    if (is_page() && is_page_breadcrumbs_position_footer_before()){
+      get_template_part('tmp/breadcrumbs-page');
+    } ?>
+
+    <footer id="footer" class="footer footer-container nwa" itemscope itemtype="https://schema.org/WPFooter">
+
+      <div id="footer-in" class="footer-in wrap cf">
+
+        <?php //フッターにウィジェットが一つも入っていない時とモバイルの時は表示しない
+        if ( (is_active_sidebar('footer-left') ||
+          is_active_sidebar('footer-center') ||
+          is_active_sidebar('footer-right') )  ): ?>
+          <div class="footer-widgets cf">
+             <div class="footer-left">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-left') ) : else : ?>
+             <?php endif; ?>
+             </div>
+             <div class="footer-center">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-center') ) : else : ?>
+             <?php endif; ?>
+             </div>
+             <div class="footer-right">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-right') ) : else : ?>
+             <?php endif; ?>
+             </div>
+          </div>
+        <?php endif; ?>
+
+        <?php //モバイルウィジェット
+        if (is_active_sidebar('footer-mobile')): ?>
+          <div class="footer-widgets-mobile cf">
+             <div class="footer-mobile">
+             <?php dynamic_sidebar('footer-mobile'); ?>
+             </div>
+          </div>
+        <?php endif ?>
+
+        <?php //フッターの最下部（フッターメニューやクレジットなど）
+        get_template_part('tmp/footer-bottom'); ?>
+
+      </div>
+
+    </footer>
+
+    <?php //管理者用パネル
+    get_template_part('tmp/admin-panel'); ?>
+
+    <?php //モバイルヘッダーメニューボタン
+    get_template_part('tmp/mobile-header-menu-buttons'); ?>
+
+    <?php //モバイルフッターメニューボタン
+    get_template_part('tmp/mobile-footer-menu-buttons'); ?>
+
+    <?php //トップへ戻るボタンテンプレート
+    get_template_part('tmp/button-go-to-top'); ?>
+
+    <?php if (!is_amp()) {
+      //再利用用にフッターコードを取得
+      //wp_footer()関数では、一度しか出力が行われないようなので事前にグローバル変数に格納しておく
+      global $_WP_FOOTER;
+      ob_start();
+      wp_footer();
+      $f = ob_get_clean();
+      echo $f;
+      $_WP_FOOTER = $f;
+    } ?>
+
+    <?php //フッターで読み込むscriptをまとめたもの
+    get_template_part('tmp/footer-scripts');?>
+
+  </div><!-- #container -->
+
+  <?php //barba.js処理
+  get_template_part('tmp/footer-barba-js');?>
+
+  <script>
+    (function(window, document) {
+      function adsenseLoad() {
+        // GoogleAdSense読込み
+        var ad = document.createElement('script');
+        ad.type = 'text/javascript';
+        ad.async = true;
+        ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+        var sc = document.getElementsByTagName('script')[0];
+        sc.parentNode.insertBefore(ad, sc);
+      }
+      // 遅延読込み
+      var lazyLoad = false;
+      function onLazyLoad() {
+        if (lazyLoad === false) {
+          // 複数呼び出し回避 + イベント解除
+          lazyLoad = true;
+          window.removeEventListener('scroll', onLazyLoad);
+          window.removeEventListener('mousemove', onLazyLoad);
+          window.removeEventListener('mousedown', onLazyLoad);
+          window.removeEventListener('touchstart', onLazyLoad);
+          window.removeEventListener('keydown', onLazyLoad);
+          adsenseLoad();
+        }
+      }
+      window.addEventListener('scroll', onLazyLoad);
+      window.addEventListener('mousemove', onLazyLoad);
+      window.addEventListener('mousedown', onLazyLoad);
+      window.addEventListener('touchstart', onLazyLoad);
+      window.addEventListener('keydown', onLazyLoad);
+      window.addEventListener('load', function() {
+        // ドキュメント途中（更新時 or ページ内リンク）
+        if (window.pageYOffset) {
+          onLazyLoad();
+        }
+      });
+    })(window, document);
+  </script>
+</body>
+
+</html>
--- functions.php
+++ functions.php
@@ -29,4 +29,58 @@ add_filter( 'upload_mimes', 'custom_mime_types' );
         return $result;
     }
     add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
+
+//jsにasync属性を付与（ページ読み込みスピードアップ対策）
+if (!(is_admin() )) {
+  function add_async_to_enqueue_script( $url ) {
+  if ( FALSE === strpos( $url, '.js' ) ) return $url;
+  if ( strpos( $url, 'jquery.js' ) ) return $url;       //対象外
+  if ( strpos( $url, 'jquery.min.js' ) ) return $url;
+  return "$url' async charset='UTF-8";        // async属性を付与
+}
+add_filter( 'clean_url', 'add_async_to_enqueue_script', 11, 1 );
+}
+
+if ( !function_exists( 'is_footer_javascript_enable' ) ):
+    function is_footer_javascript_enable(){
+      return true;//get_theme_option(OP_FOOTER_JAVASCRIPT_ENABLE, 1);
+    }
+endif;
+
+//サイトフォントソースフォントウェイト（太さ）の取得
+if ( !function_exists( 'get_site_font_source_weight' ) ):
+    function get_site_font_source_weight(){
+      switch (get_site_font_family()) {
+        case 'noto_sans_jp':
+          //$font_source_weight = ':100,300,400,500,700,900';
+          $font_source_weight = ':400,700';
+          break;
+        case 'noto_serif_jp':
+          $font_source_weight = ':200,300,400,500,600,700,900';
+          break;
+        case 'mplus_1p':
+          $font_source_weight = ':100,300,400,500,700,800,900';
+          break;
+        case 'rounded_mplus_1c':
+          $font_source_weight = ':100,300,400,500,700,800,900';
+          break;
+        case 'kosugi':
+          $font_source_weight = '';
+          break;
+        case 'kosugi_maru':
+          $font_source_weight = '';
+          break;
+        case 'sawarabi_gothic':
+          $font_source_weight = '';
+          break;
+        case 'sawarabi_mincho':
+          $font_source_weight = '';
+          break;
+        default:
+        $font_source_weight = null;
+          break;
+      }
+      return $font_source_weight;
+    }
+endif;
     
